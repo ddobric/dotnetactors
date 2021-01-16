6dbae48f94920ba3f2987f8a1b9350c78e70233c
chetan2510 <chetanchadha2594@gmail.com>
2021-01-16 11:02:46 +0100

    Created basic project directory

---
+++ dotnetactors/dotnetactors/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace dotnetactors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
