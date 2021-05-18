960f9c6960dc24b60c4f1f2a108cc5a33313129c
chetan2510 <chetanchadha2594@gmail.com>
2021-05-18 20:45:39 +0200

    updated proj file

--- source/CarSampleActor/Program.cs
+++ source/CarSampleActor/Program.cs
@@ -18,8 +18,6 @@ namespace CarSampleActor
             IConfigurationRoot configArgs = builder.Build();
             ILogger logger = generateLogger();
 
-            await LoadCarAttributes(logger);
-
             if (configArgs["shallRun"] == "true")
             {
                 await LoadCarAttributes(logger);
@@ -27,6 +25,45 @@ namespace CarSampleActor
             else
             {
                 await CheckPersistence(logger);
+            }
+        }
+        
+        static async Task Method1()
+        {
+            Console.WriteLine("Persist Car attributes started...."+Thread.CurrentThread.Name);
+            CancellationTokenSource src = new CancellationTokenSource();
+            var cfg = GetLocaSysConfig();
+            Console.WriteLine("Loaded Configuration, Messaging-Queue:"+cfg.ReplyMsgQueue+", Message-Topic:"+cfg.RequestMsgTopic);
+            ActorSystem sysLocal = new ActorSystem($"CarFunctionalityTest", cfg);
+            Console.WriteLine("Created ActorSystem");
+            
+            Console.WriteLine("Creating multiple Actor references");
+
+            for (int i = 1; i < 5000; i++)
+            {
+                Console.WriteLine("Actors"+i);
+                ActorReference actorRef1 = sysLocal.CreateActor<MyActor>(i);
+                var response =   await actorRef1.Ask<CarAttributes>(new CarAttributes() {CarColor = "green", CarSpeed = "" + (222 + i), Persisted = false});
+                Console.WriteLine("Received result: "+response.Persisted);  
+            }
+        }
+        
+        private static void method(ILogger logger)
+        {
+            logger?.LogInformation("Persist Car attributes started....");
+            CancellationTokenSource src = new CancellationTokenSource();
+            var cfg = GetLocaSysConfig();
+            logger?.LogInformation("Loaded Configuration, Messaging-Queue:"+cfg.ReplyMsgQueue+", Message-Topic:"+cfg.RequestMsgTopic);
+            ActorSystem sysLocal = new ActorSystem($"CarFunctionalityTest", cfg);
+            logger?.LogInformation("Created ActorSystem");
+            
+            logger?.LogInformation("Creating multiple Actor references");
+
+            for (int i = 1; i < 5000; i++)
+            {
+                ActorReference actorRef1 = sysLocal.CreateActor<MyActor>(i);
+                var response =   actorRef1.Ask<CarAttributes>(new CarAttributes() {CarColor = "green", CarSpeed = "" + (222 + i), Persisted = false});
+                //logger?.LogInformation("Received result: "+response.Persisted);  
             }
         }
 
@@ -58,7 +95,7 @@ namespace CarSampleActor
             ActorSystem sysLocal = new ActorSystem($"CarFunctionalityTest", cfg);
             logger?.LogInformation("Created ActorSystem");
 
-            for (int i = 1; i < 50; i++)
+            for (int i = 1; i < 5000; i++)
             {
                 ActorReference actorRef1 = sysLocal.CreateActor<MyActor>(i);
                 var response = await actorRef1.Ask<long>(i, routeToNode:"node1");
